f7be99052f21ca4d8134692e820d2a3e0a6f8018
Olivier Lauzon <olauzon@gmail.com>
2014-06-09 20:02:33 -0400

    Invert dependencies

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ;; [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [capacitor "0.2.2"]]
+                 [capacitor "0.2.2"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
+  :aot :all
   :main async-test.core)
